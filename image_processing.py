d4c5b87ac6ea1cab79c1a2ccaad9dd3b9c8e223a
yui1337 <egolunov095@gmail.com>
2024-10-25 22:26:33 +0400

    Added make_inverted_image, show_two_images, save_inverted_images functions

--- image_processing.py
+++ image_processing.py
@@ -2,6 +2,8 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
 
+import os
+
 
 def get_image(image_path: str) -> np.ndarray:
     img = cv.imread(image_path)
@@ -18,9 +20,9 @@ def make_histogram(img: np.ndarray) -> None:
     colors = ["r", "g", "b"]
 
     plt.figure()
-    plt.title("'Flattened' Color Histogram")
-    plt.xlabel("Bins")
-    plt.ylabel("# of Pixels")
+    plt.title("Histogram of original image")
+    plt.xlabel("Brightness")
+    plt.ylabel("Number of Pixels")
 
     for (channel, color) in zip(channels, colors):
         # create a histogram for the current channel and plot it
@@ -28,6 +30,27 @@ def make_histogram(img: np.ndarray) -> None:
         plt.plot(histogram, color=color)
         plt.xlim([0, 256])
 
+def make_inverted_image(img: np.ndarray) -> np.ndarray:
+    return cv.bitwise_not(img)
 
+def show_two_images(img: np.ndarray, inv_img: np.ndarray) -> None:
+    plt.figure()
+
+    plt.subplot(2, 1, 1)
+    plt.imshow(img)
+    plt.title("Original image")
+    plt.axis('off')
+
+    plt.subplot(2, 1, 2)
+    plt.imshow(inv_img)
+    plt.title("Inverted image")
+    plt.axis('off')
+
+def save_inverted_image(save_dir: str, img: np.ndarray, filename: str) -> None:
+    if not (os.path.isdir(save_dir)):
+        os.mkdir(save_dir)
+    filename = filename + ".jpg"
+    save_dir = os.path.join(save_dir, filename)
+    cv.imwrite(save_dir, img)
 
 
