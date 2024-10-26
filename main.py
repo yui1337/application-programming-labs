92d361b776f77daac5ee7fcb4edd7112ee0e664a
yui1337 <egolunov095@gmail.com>
2024-10-26 21:26:28 +0400

    Edited argument parser and main() corresponding to changes in save_image function. Added exceptions handlings.

--- main.py
+++ main.py
@@ -10,23 +10,31 @@ def get_args() -> tuple[str, str, str]:
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--image_path", type=str, help="Path to the image you'd like to invert")
     parser.add_argument("-s", "--save_dir", type=str, help="Directory you'd like to save inverted image")
-    parser.add_argument("-n", "--new_filename", type=str, help="Name of inverted image")
     arguments = parser.parse_args()
     return arguments.image_path, arguments.save_dir, arguments.new_filename
 
 
 def main():
-    img_path, save_dir, new_filename = get_args()
-
-    img = get_image(img_path)
-    print(get_image_dimensions(img))
-
-    r_hist, g_hist, b_hist = make_histogram(img)
-    draw_histogram(r_hist, g_hist, b_hist)
-    inv_img = make_inverted_image(img)
-    save_image(save_dir, inv_img, new_filename)
-    show_two_images(img,inv_img)
-    plt.show()
+    img_path, save_dir= get_args()
+    try:
+        img = get_image(img_path)
+        print(get_image_dimensions(img))
+    except Exception:
+        print("Cant read image. Check your input.")
+        exit()
+    if img is not(None):
+        r_hist, g_hist, b_hist = make_histogram(img)
+        draw_histogram(r_hist, g_hist, b_hist)
+        inv_img = make_inverted_image(img)
+        try:
+            save_image(save_dir, inv_img)
+        except Exception as e:
+            print(f"Something went wrong:{e}. Try to input path that ends with '.YYY', where YYY - needed format of images")
+        show_two_images(img,inv_img)
+        plt.show()
+    else:
+        print("Something went wrong. Can't process image.")
+        exit()
 
 
 if __name__ == "__main__":
