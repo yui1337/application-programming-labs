c9846641fcd559d8f4a2f92dc3ccdca33cc93a50
yui1337 <egolunov095@gmail.com>
2024-11-10 13:13:09 +0400

    Beautify main; Added exception; PEP-8

--- main.py
+++ main.py
@@ -15,10 +15,31 @@ def get_args() -> str:
     arguments = parser.parse_args()
     return arguments.csv
 
+
 def main() -> None:
     csv_path = get_args()
-    df = make_df(csv_path)
+    try:
+        df = make_df(csv_path)
+        print("DataFrame from csv \n", df)
 
+        add_new_columns(df)
+        print("Added width, height, channels columns \n", df)
+
+        info = stat_info(df)
+        print("Statistical information of DataFrame \n", info)
+
+        filtered = filter_by_sizes(df, 1000, 1000)
+        print("DataFrame filtered by max width, max_height \n", filtered)
+
+        add_area(df)
+        print("Added area column \n", df)
+
+        sorted = sort_by_area(df)
+        print("DataFrame sorted by area(from smaller to larger) \n", df)
+
+        create_hist_by_area(df)
+    except Exception as e:
+        print(f"Something went wrong: {e}")
 
 
 if __name__ == "__main__":
