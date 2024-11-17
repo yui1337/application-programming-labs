9b54512d699c092a8c56145abf22aaca7f45bb9e
yui1337 <egolunov095@gmail.com>
2024-11-17 14:31:28 +0400

    Added set_iterator, next_img, display error methods. Changed structure of UI, deleted unnecessary settings of certain elements.

--- dataset_viewer.py
+++ dataset_viewer.py
@@ -1,153 +1,127 @@
-from PyQt5.QtWidgets import QFileDialog
+import os
+
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt6.QtWidgets import QMainWindow, QApplication
+
 from imgiterator import ImageIterator
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        self.counter = 0
-        self.iterator = None
-        self.csv_path = ""
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1280, 720)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
-        MainWindow.setSizePolicy(sizePolicy)
-        MainWindow.setMinimumSize(QtCore.QSize(854, 480))
-        MainWindow.setMaximumSize(QtCore.QSize(2560, 1440))
-        MainWindow.setStyleSheet("background-color: rgb(43, 45, 48);\n"
-                                 "")
+class UiMainWindow(object):
+    def setup_ui(self, main_window: QMainWindow) -> None:
+        """
+        Creates User Interface
+        :param main_window: QMainWindow object
+        """
+        main_window.setObjectName("main_window")
+        main_window.setWindowTitle("Dataset Viewer")
+        main_window.setFixedSize(1280, 720)
+        main_window.setStyleSheet("background-color: rgb(43, 45, 48);")
 
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
+        self.container = QtWidgets.QWidget(main_window)
 
-        self.image = QtWidgets.QLabel(self.centralwidget)
+        self.container.setObjectName("container")
+
+        self.image = QtWidgets.QLabel(self.container)
         self.image.setGeometry(QtCore.QRect(260, 80, 1000, 600))
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.image.sizePolicy().hasHeightForWidth())
-        self.image.setSizePolicy(sizePolicy)
-        #self.image.setLayoutDirection(QtCore.Qt.RightToLeft)
-        self.image.setText("")
-        self.image.setPixmap(QtGui.QPixmap("../../../../../images/000001.jpg"))
-        self.image.setScaledContents(False)
-        #self.image.setAlignment(QtCore.Qt.AlignCenter)
-        self.image.setWordWrap(False)
-        self.image.setOpenExternalLinks(False)
-        #self.image.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
+        self.image.setText("Waiting for input...")
+        self.image.setAlignment(QtCore.Qt.AlignCenter)
         self.image.setObjectName("image")
-        self.widget = QtWidgets.QWidget(self.centralwidget)
-        self.widget.setGeometry(QtCore.QRect(10, 50, 240, 660))
-        self.widget.setObjectName("widget")
+        self.image.setStyleSheet("color: rgb(111, 115, 122);\n"
+                                   "font: 18pt \"Roboto\";\n")
 
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.widget)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout.setSpacing(100)
-        self.verticalLayout.setObjectName("verticalLayout")
+        self.btns_col = QtWidgets.QWidget(self.container)
+        self.btns_col.setGeometry(QtCore.QRect(10, 50, 240, 660))
+        self.btns_col.setObjectName("btns_col")
+        self.btns_layout = QtWidgets.QVBoxLayout(self.btns_col)
+        self.btns_layout.setContentsMargins(0, 0, 0, 0)
+        self.btns_layout.setSpacing(100)
+        self.btns_layout.setObjectName("btns_layout")
 
-        self.btn_choose_csv = QtWidgets.QPushButton(self.widget)
-        self.btn_choose_csv.setEnabled(True)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.btn_choose_csv.sizePolicy().hasHeightForWidth())
-        self.btn_choose_csv.setSizePolicy(sizePolicy)
-        #self.btn_choose_csv.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.btn_choose_csv.setStyleSheet("font: 20pt \"Roboto\";\n"
+        self.btn_choose_csv = QtWidgets.QPushButton(self.btns_col)
+        self.btn_choose_csv.setStyleSheet("font: 18pt \"Roboto\";\n"
                                           "background-color: rgb(43, 45, 48);\n"
                                           "border: 2px solid rgb(67, 69, 74);\n"
                                           "border-radius: 5px;\n"
                                           "color: rgb(111, 115, 122);")
-        self.btn_choose_csv.setDefault(False)
-        self.btn_choose_csv.setFlat(False)
+        self.btn_choose_csv.setText("Choose CSV file")
         self.btn_choose_csv.setObjectName("btn_choose_csv")
-        self.verticalLayout.addWidget(self.btn_choose_csv)
+        self.btns_layout.addWidget(self.btn_choose_csv)
 
-        self.btn_next_img = QtWidgets.QPushButton(self.widget)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.btn_next_img.sizePolicy().hasHeightForWidth())
-        self.btn_next_img.setSizePolicy(sizePolicy)
-        self.btn_next_img.setStyleSheet("font: 20pt \"Roboto\";\n"
+        self.btn_next_img = QtWidgets.QPushButton(self.btns_col)
+        self.btn_next_img.setStyleSheet("font: 18pt \"Roboto\";\n"
                                         "background-color: rgb(43, 45, 48);\n"
                                         "border: 2px solid rgb(67, 69, 74);\n"
                                         "border-radius: 5px;\n"
                                         "color: rgb(111, 115, 122);")
         self.btn_next_img.setObjectName("btn_next_img")
-        self.verticalLayout.addWidget(self.btn_next_img)
+        self.btn_next_img.setText("Next >")
+        self.btns_layout.addWidget(self.btn_next_img)
 
-        self.btn_exit = QtWidgets.QPushButton(self.widget)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.btn_exit.sizePolicy().hasHeightForWidth())
-        self.btn_exit.setSizePolicy(sizePolicy)
-        self.btn_exit.setStyleSheet("font: 20pt \"Roboto\";\n"
+        self.btn_exit = QtWidgets.QPushButton(self.btns_col)
+        self.btn_exit.setStyleSheet("font: 18pt \"Roboto\";\n"
                                     "background-color: rgb(43, 45, 48);\n"
                                     "border: 2px solid rgb(67, 69, 74);\n"
                                     "border-radius: 5px;\n"
                                     "color: rgb(111, 115, 122);")
         self.btn_exit.setObjectName("btn_exit")
-        self.verticalLayout.addWidget(self.btn_exit)
+        self.btn_exit.setText("Exit")
+        self.btns_layout.addWidget(self.btn_exit)
 
-        self.widget1 = QtWidgets.QWidget(self.centralwidget)
-        self.widget1.setGeometry(QtCore.QRect(530, 20, 453, 46))
-        self.widget1.setObjectName("widget1")
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget1)
+        self.img_title = QtWidgets.QLabel(self.container)
+        self.img_title.setGeometry(QtCore.QRect(530, 20, 453, 46))
+        self.img_title.setObjectName("img_title")
+        self.img_title.setStyleSheet("color: rgb(111, 115, 122);\n"
+                                     "font: 18pt \"Roboto\";\n")
 
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout.setSpacing(10)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.label_3 = QtWidgets.QLabel(self.widget1)
-        self.label_3.setStyleSheet("color: rgb(111, 115, 122);\n"
-                                   "font: 20pt \"Roboto\";\n"
-                                   "border-color: rgb(67, 68, 90);")
-        #self.label_3.setTextFormat(QtCore.Qt.PlainText)
-        #self.label_3.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_3.setObjectName("label_3")
-        self.horizontalLayout.addWidget(self.label_3)
+        main_window.setCentralWidget(self.container)
 
-        self.current_img = QtWidgets.QLabel(self.widget1)
-        self.current_img.setStyleSheet("color: rgb(111, 115, 122);\n"
-                                       "font: 20pt \"Roboto\";\n"
-                                       "border-color: rgb(67, 68, 90);")
-       # self.current_img.setTextFormat(QtCore.Qt.PlainText)
-        #self.current_img.setAlignment(QtCore.Qt.AlignCenter)
-        self.current_img.setObjectName("current_img")
-        self.horizontalLayout.addWidget(self.current_img)
-        MainWindow.setCentralWidget(self.centralwidget)
+        QtCore.QMetaObject.connectSlotsByName(main_window)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.btn_choose_csv.clicked.connect(self.load_csv)
+        self.btn_next_img.clicked.Adconnect(self.next_img)
 
-        self.btn_choose_csv.clicked.connect(lambda: self.load_csv())
-        self.btn_next_img.clicked.connect(lambda: self.next_img(self))
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Просмотр датасета"))
-        self.btn_choose_csv.setText(_translate("MainWindow", "Choose CSV file"))
-        self.btn_next_img.setText(_translate("MainWindow", "Next >"))
-        self.btn_exit.setText(_translate("MainWindow", "Exit"))
-        self.label_3.setText(_translate("MainWindow", "Current image:"))
-        self.current_img.setText(_translate("MainWindow", "image name"))
-
-    def load_csv(self):
-        filename, _ = QFileDialog.getOpenFileName(None, "Select a File", "", "CSV file (*.csv)")
-        if filename:
-            self.csv_path = filename
-            # self.set_viewimages_menu()
-
-    def next_img(self):
+    def set_iterator(self) -> None:
+        """
+        Creates ImageIterator iterator
+        """
         self.iterator = ImageIterator(self.csv_path)
         iter(self.iterator)
-        next_img_path = next(self.iterator)
-        self.image.setPixmap(QtGui.QPixmap(next_img_path[1]))
-        self.counter += 1
 
+    def load_csv(self) -> None:
+        """
+        Opens file dialog window and loads first image from dataset
+        """
+        filename, _ = QFileDialog.getOpenFileName(self.container, "Select a File", "", "CSV file (*.csv)")
+        try:
+            self.csv_path = filename
+            self.set_iterator()
+            self.next_img()
+        except:
+            self.display_error("Something went wrong! File wasn't opened.")
 
+    def next_img(self) -> None:
+        """
+        Changes current displaying image and its title
+        """
+        try:
+            next_img_path = next(self.iterator)
+            self.image.setPixmap(QtGui.QPixmap(next_img_path).scaled(self.image.size(), QtCore.Qt.KeepAspectRatio))
+            self.img_title.setText("Current image: " + os.path.basename(next_img_path))
+        except StopIteration:
+            self.set_iterator()
+            self.next_img()
+        except:
+            self.display_error("Can't show image! Maybe you haven't loaded CSV or "
+                               "dataset is not valid.")
 
+    def display_error(self, text: str) -> None:
+        """
+        Pops error window
+        :param text: description of error
+        """
+        err = QMessageBox()
+        err.setWindowTitle("Error!")
+        err.setText(text)
+        err.setIcon(QMessageBox.Warning)
+        err.exec()
